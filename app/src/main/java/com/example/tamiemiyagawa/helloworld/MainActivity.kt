e64778fb89e28c78364afc440575bd4ff73d7bda
tam1g <tamie.off@gmail.com>
2019-02-18 20:02:28 +0900

    エラーはあるのですが

--- app/src/main/java/com/example/tamiemiyagawa/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/tamiemiyagawa/helloworld/MainActivity.kt
@@ -4,9 +4,40 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val result = findViewById(R.id.textView3) as TextView
+        val btnAdd = findViewById(R.id.button) as Button
+        val btnMinus = findViewById(R.id.button2) as Button
+        val btnMul = findViewById(R.id.button3) as Button
+        val btnDiv = findViewById(R.id.button4) as Button
+
+        btnAdd.setOnClickListener {
+            result.text = (getNumber() + getNumberTwo()).toString()
+        }
+        btnMinus.setOnClickListener {
+            result.text = "0"
+            result.text = (getNumber() - getNumberTwo()).toString()
+        }
+        btnMul.setOnClickListener {
+            result.text = "0"
+            result.text = (getNumber() * getNumberTwo()).toString()
+        }
+        btnDiv.setOnClickListener {
+            result.text = "0"
+            result.text = (getNumber() / getNumberTwo()).toString()
+        }
+    }
+
+    fun getNumber() : Int{
+        val inputNumber = findViewById(R.id.editText) as EditText
+        return Integer.parseInt(inputNumber.text.toString())
+    }
+
+    fun getNumberTwo() : Int{
+        val inputNumberTwo = findViewById(R.id.editText2) as EditText
+        return Integer.parseInt(inputNumberTwo.text.toString())
     }
 }
